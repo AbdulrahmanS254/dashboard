6dcd330f4185b5693e5ea97355425622eee29a05
Abdulrahman Shawky <hawk18922@gmail.com>
2026-01-05 19:21:13 +0200

    shift in direction

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+// Supports weights 100-900
+import "@fontsource-variable/dm-sans";
+// Supports weights 400-900
+import "@fontsource-variable/cinzel";
+
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from "./pages/Home/Home.jsx";
 import ProductDetails from "./pages/ProductDetails/ProductDetails.jsx";
+
+import "./index.css";
 
 const router = createBrowserRouter([
     {
@@ -25,5 +32,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 );
